9316f7ec2ea741d923994b211a878590e518e24c
Abdull AL Mamun (PARVEJ) <107097395+Parvej0099@users.noreply.github.com>
2022-07-27 13:46:50 +0600

    Update omni_X655C.mk

--- omni_X655C.mk
+++ omni_X655C.mk
@@ -24,12 +24,6 @@ PRODUCT_MANUFACTURER := infinix
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 29
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 29
-
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
